ad158b47f9059de680a78783d765e6b5a0ddd2fa
dzmitry_dp <dzmitrupaulenka@outlook.com>
2022-12-25 18:36:03 +0100

    Creade analyse obj

--- map.py
+++ map.py
@@ -2,20 +2,30 @@
 # Составляю карту ссылок
 # Вывожу список количества упоминаний ИО в текстах
 
-import os, sys
-import pandas as pd
+import os
 
 from files import Tensor # объект .md файла
 from folders import Folder # объект каталогов
-from intel_obj import get_all_obj # собрать объекты всех наборов символов, которые буду считать словом
+from analysis import FramesData # объект таблиц
 
 
-def refresh_relation(path=os.getcwd()) -> list:
+def _sort(A: list) -> None:
     """
-    В каждом каталоге местонахождения скрипта ищем marckdown файлы.
+    Сортировка объектов списка А по количеству вложений. 
+    Те файлы что ближе к корню запуска скрипта первые
+    Те файлы что лежат в далеких папках - последние
     """
-    # tensors.clear()
-    # folders.clear()
+    N = len(A)
+    for bypass in range(1, N):
+        for k in range(0, N-bypass):
+            if len(A[k].root.split('\\')) > len(A[k+1].root.split('\\')):
+                A[k], A[k+1] = A[k+1], A[k]
+
+def refresh_relation(path=os.getcwd()):
+    """
+    В каждом каталоге от местонахождения скрипта ищем marckdown файлы
+    Наполняю списки tensors и folders объектами
+    """
     files_and_folders_list = os.listdir(path)
 
     for file_or_catalog in files_and_folders_list:
@@ -28,34 +38,23 @@ def refresh_relation(path=os.getcwd()) -> list:
             folder_path = path + f'\\{folder}'
             refresh_relation(folder_path)
             folders.append(Folder(folder_path))
-    
-    def _sort(A: list) -> None:
-        """
-        Сортировка объектов списка А по количеству вложений. 
-        Те файлы что ближе к корню запуска скрипта первые
-        Те файлы что лежат в далеких папках - последние
-        """
-        N = len(A)
-        for bypass in range(1, N):
-            for k in range(0, N-bypass):
-                if len(A[k].root.split('\\')) > len(A[k+1].root.split('\\')):
-                    A[k], A[k+1] = A[k+1], A[k]
+
+if __name__ == '__main__':
+    tensors: list[Tensor] = [] # все объекты файлов .md
+    folders: list[Folder] = [] # все объекты обобщающих каталогов/папок
+    refresh_relation() # обновляю списки tensors, folders
 
     _sort(tensors)
     _sort(folders)
 
-if __name__ == '__main__':
-    tensors = [] # все объекты файлов .md
-    folders = [] # все объекты обобщающих каталогов/папок
-    refresh_relation()
-
-    d = get_all_obj(tensors)
-    df = pd.DataFrame(dict([ (k, pd.Series(v)) for k,v in d.items() ]))
-    print(df.head(7))
+    frames = FramesData(tensors) # таблицы данных
 
     print('---')
-    for variable in dir():
-        if '__' != variable[:2] and '__' != variable[-2:]:
-            print(f'> {variable}')
+    print("""
+    tensors - список объектов, созданных по файлам .md
+    folders - список объектов, созданных по каталогам
+    frames - объект для анализа полученных объектов
+        .symbols - таблица соответвия набору символов и его объекту
+    """)
     print('---')
     
